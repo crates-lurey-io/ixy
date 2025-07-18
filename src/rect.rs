c027fb73dcfa8f1fd7ac28cbf7fe1c6eb2effe25
Matan Lurey <matan@lurey.org>
2025-07-17 18:59:57 -0700

    Add Rect::intersect

--- src/rect.rs
+++ src/rect.rs
@@ -347,6 +347,37 @@ impl<T: Int> Rect<T> {
         self.contains(other.l, other.t) && self.contains(other.r, other.b)
     }
 
+    /// Returns the intersection of this rectangle with another rectangle.
+    ///
+    /// If the rectangles do not overlap, returns an empty rectangle.
+    ///
+    /// ## Examples
+    ///
+    /// ```rust
+    /// use ixy::Rect;
+    ///
+    /// let a = Rect::from_ltrb(1, 2, 5, 6).unwrap();
+    /// let b = Rect::from_ltrb(3, 4, 7, 8).unwrap();
+    /// let intersection = a.intersect(b);
+    /// assert_eq!(intersection, Rect::from_ltrb(3, 4, 5, 6).unwrap());
+    ///
+    /// let c = Rect::from_ltrb(6, 7, 8, 9).unwrap();
+    /// assert_eq!(a.intersect(c), Rect::EMPTY);
+    /// ```
+    #[must_use]
+    pub fn intersect(&self, other: Rect<T>) -> Rect<T> {
+        let l = core::cmp::max(self.l, other.l);
+        let t = core::cmp::max(self.t, other.t);
+        let r = core::cmp::min(self.r, other.r);
+        let b = core::cmp::min(self.b, other.b);
+
+        if l < r && t < b {
+            Rect { l, t, r, b }
+        } else {
+            Rect::EMPTY
+        }
+    }
+
     /// Returns an iterator over the positions within the rectangle, in row-major order.
     ///
     /// The positions are exclusive of the bottom-right edge.
@@ -691,5 +722,29 @@ mod tests {
                 Pos::new(2, 5)
             ]
         );
+    }
+
+    #[test]
+    fn intersect_full() {
+        let a = Rect::from_ltrb(1, 2, 5, 6).unwrap();
+        let b = Rect::from_ltrb(1, 2, 5, 6).unwrap();
+        let intersection = a.intersect(b);
+        assert_eq!(intersection, a);
+    }
+
+    #[test]
+    fn intersect_partial() {
+        let a = Rect::from_ltrb(1, 2, 5, 6).unwrap();
+        let b = Rect::from_ltrb(3, 4, 7, 8).unwrap();
+        let intersection = a.intersect(b);
+        assert_eq!(intersection, Rect::from_ltrb(3, 4, 5, 6).unwrap());
+    }
+
+    #[test]
+    fn intersect_none() {
+        let a = Rect::from_ltrb(1, 2, 5, 6).unwrap();
+        let b = Rect::from_ltrb(6, 7, 8, 9).unwrap();
+        let intersection = a.intersect(b);
+        assert_eq!(intersection, Rect::EMPTY);
     }
 }
