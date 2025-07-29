7641628e88a29a4cc7848b9c6f60e85ca184efb2
Matan Lurey <matan@lurey.org>
2025-07-29 16:53:35 -0700

    Add examples, some math methods for Size

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
 //! A terse, no-std crate for 2D integer geometry.
+//!
+//! ## Examples
+//!
+//! ```rust
+//! use ixy::{Pos, Rect};
+//!
+//! let pos = Pos::new(10, 20);
+//! let rect = Rect::from_ltwh(0, 0, 100, 200);
+//!
+//! assert_eq!(pos.x, 10);
+//! assert_eq!(pos.y, 20);
+//! assert_eq!(rect.left(), 0);
+//! assert_eq!(rect.top(), 0);
+//! assert_eq!(rect.width(), 100);
+//! assert_eq!(rect.height(), 200);
+//! assert_eq!(rect.right(), 100);
+//! assert_eq!(rect.bottom(), 200);
+//! assert!(rect.contains_pos(pos));
+//! assert!(!rect.contains_pos(Pos::new(150, 250)));
+//! ```
 
 #![no_std]
 
--- src/size.rs
+++ src/size.rs
@@ -1,3 +1,5 @@
+use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
 use crate::Rect;
 
 /// Represents a size in 2D space, with `width` and `height`.
@@ -15,6 +17,114 @@ impl Size {
     #[must_use]
     pub const fn new(width: usize, height: usize) -> Self {
         Self { width, height }
+    }
+}
+
+impl Add for Size {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            width: self.width + other.width,
+            height: self.height + other.height,
+        }
+    }
+}
+
+impl AddAssign for Size {
+    fn add_assign(&mut self, other: Self) {
+        self.width += other.width;
+        self.height += other.height;
+    }
+}
+
+impl Sub for Size {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            width: self.width - other.width,
+            height: self.height - other.height,
+        }
+    }
+}
+
+impl SubAssign for Size {
+    fn sub_assign(&mut self, other: Self) {
+        self.width -= other.width;
+        self.height -= other.height;
+    }
+}
+
+impl Mul for Size {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            width: self.width * other.width,
+            height: self.height * other.height,
+        }
+    }
+}
+
+impl Mul<usize> for Size {
+    type Output = Self;
+
+    fn mul(self, scalar: usize) -> Self::Output {
+        Self {
+            width: self.width * scalar,
+            height: self.height * scalar,
+        }
+    }
+}
+
+impl MulAssign for Size {
+    fn mul_assign(&mut self, other: Self) {
+        self.width *= other.width;
+        self.height *= other.height;
+    }
+}
+
+impl MulAssign<usize> for Size {
+    fn mul_assign(&mut self, scalar: usize) {
+        self.width *= scalar;
+        self.height *= scalar;
+    }
+}
+
+impl Div for Size {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self::Output {
+        Self {
+            width: self.width / other.width,
+            height: self.height / other.height,
+        }
+    }
+}
+
+impl Div<usize> for Size {
+    type Output = Self;
+
+    fn div(self, scalar: usize) -> Self::Output {
+        Self {
+            width: self.width / scalar,
+            height: self.height / scalar,
+        }
+    }
+}
+
+impl DivAssign for Size {
+    fn div_assign(&mut self, other: Self) {
+        self.width /= other.width;
+        self.height /= other.height;
+    }
+}
+
+impl DivAssign<usize> for Size {
+    fn div_assign(&mut self, scalar: usize) {
+        self.width /= scalar;
+        self.height /= scalar;
     }
 }
 
@@ -67,5 +177,95 @@ mod tests {
         assert_eq!(rect.top(), 0);
         assert_eq!(rect.right(), 10);
         assert_eq!(rect.bottom(), 20);
+    }
+
+    #[test]
+    fn add_size_size() {
+        let size1 = Size::new(10, 20);
+        let size2 = Size::new(5, 15);
+        let result = size1 + size2;
+        assert_eq!(result, Size::new(15, 35));
+    }
+
+    #[test]
+    fn add_assign_size() {
+        let mut size = Size::new(10, 20);
+        size += Size::new(5, 15);
+        assert_eq!(size, Size::new(15, 35));
+    }
+
+    #[test]
+    fn sub_size_size() {
+        let size1 = Size::new(10, 20);
+        let size2 = Size::new(5, 15);
+        let result = size1 - size2;
+        assert_eq!(result, Size::new(5, 5));
+    }
+
+    #[test]
+    fn sub_assign_size() {
+        let mut size = Size::new(10, 20);
+        size -= Size::new(5, 15);
+        assert_eq!(size, Size::new(5, 5));
+    }
+
+    #[test]
+    fn mul_size_size() {
+        let size1 = Size::new(10, 20);
+        let size2 = Size::new(2, 3);
+        let result = size1 * size2;
+        assert_eq!(result, Size::new(20, 60));
+    }
+
+    #[test]
+    fn mul_assign_size() {
+        let mut size = Size::new(10, 20);
+        size *= Size::new(2, 3);
+        assert_eq!(size, Size::new(20, 60));
+    }
+
+    #[test]
+    fn mul_size_scalar() {
+        let size = Size::new(10, 20);
+        let scalar = 2;
+        let result = size * scalar;
+        assert_eq!(result, Size::new(20, 40));
+    }
+
+    #[test]
+    fn mul_assign_size_scalar() {
+        let mut size = Size::new(10, 20);
+        size *= 2;
+        assert_eq!(size, Size::new(20, 40));
+    }
+
+    #[test]
+    fn div_size_size() {
+        let size1 = Size::new(10, 20);
+        let size2 = Size::new(2, 4);
+        let result = size1 / size2;
+        assert_eq!(result, Size::new(5, 5));
+    }
+
+    #[test]
+    fn div_assign_size() {
+        let mut size = Size::new(10, 20);
+        size /= Size::new(2, 4);
+        assert_eq!(size, Size::new(5, 5));
+    }
+
+    #[test]
+    fn div_size_scalar() {
+        let size = Size::new(10, 20);
+        let scalar = 2;
+        let result = size / scalar;
+        assert_eq!(result, Size::new(5, 10));
+    }
+
+    #[test]
+    fn div_assign_size_scalar() {
+        let mut size = Size::new(10, 20);
+        size /= 2;
+        assert_eq!(size, Size::new(5, 10));
     }
 }
